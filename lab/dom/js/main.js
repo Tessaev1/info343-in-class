3420a3a6a2f7ac9012013852ee6ea585ceb450c7
Tessaev1 <tessaev1@uw.edu>
2016-10-18 15:33:37 -0700

    in-class animation/transition exercise

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,37 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+
+boxOne.addEventListener("click", function() {
+	boxOne.classList.add("fade-to-black");
+	boxOne.classList.add("black");
+});
+
+boxTwo.addEventListener("click", function() {
+	boxTwo.classList.add("fall");
+});
+
+boxThree.addEventListener("mouseover", function() {
+	boxThree.classList.add("fade-to-white");
+	boxThree.classList.add("white");
+});
+
+boxThree.addEventListener("mouseout", function() {
+	boxThree.classList.add("fade-to-bg-color");
+	boxThree.classList.remove("white");
+	boxThree.classList.add("grey");
+});
+
+boxFour.addEventListener("click", function() {
+	boxFour.classList.add("fall");
+	boxFour.addEventListener("click", function() {
+		boxFour.classList.remove("fall");
+		boxFour.classList.add("float");
+	})
+});
+
+boxFive.addEventListener(setInterval(function() {
+	boxFive.style.height = 300;
+	boxFive.style.width = 300;
+}, 3000));
